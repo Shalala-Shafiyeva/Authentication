8d03394d328e767c3033474fecd648012e8b046e
Shalala Shafiyeva <shalala.shafiyeva23@gmail.com>
2022-12-01 13:10:44 +0400

    Add project files.

---
+++ Authentication/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Authentication
+{
+    public class UserDto
+    {
+        //This DTo for registration
+        public string UserName { get; set; }=string.Empty;
+        public string Password { get; set; }=string.Empty;  
+
+    }
+}
